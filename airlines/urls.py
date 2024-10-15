72904fb8c3fd3f86180f43dd56a39dc35dae0c28
ibrahimardic <ibrahimard2000@gmail.com>
2024-10-15 12:16:52 +0300

    Replaced static values with database values

--- airlines/urls.py
+++ airlines/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.Homepage, name="homepage"), # Root domain
-    path('airline/<str:pk>/', views.Airline, name="airline"), # airline listing
-    path('aircraft/<str:pk>/', views.AirCraft, name="aircraft"), # aircraft listing
+    path('airline/<str:pk>/', views.AirlineView, name="airline"), # airline listing
+    path('aircraft/<str:pk>/', views.AirCraftView, name="aircraft"), # aircraft listing
     path('api-token-auth/', views.ApiTokenAuth, name="api-token-auth"), # api-token-auth
-    path('airline/', views.Airlines, name="api-token-auth"),
-    path('aircraft/', views.AirCrafts, name="api-token-auth"),
+    path('airline/', views.AirlinesView, name="api-token-auth"),
+    path('aircraft/', views.AirCraftsView, name="api-token-auth"),
 ]
--- airlines/views.py
+++ airlines/views.py
@@ -1,53 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-airlines_list = [
-    {
-    'id' :'1',
-    'name': 'Pegasus',
-    'callsign': 'PC',
-    'founded_year' : 1990,
-    'base_airport': 'SAW'
-    }
-]
-aircrafts_list = [
-    {
-        'id':'1',
-        'manufacturer_serial_number':'4629',
-        'type':'Airbus',
-        'model':'Airbus A319-132',
-        'operator_airline':'1',
-        'number_of_engines':2,
-
-    }
-]
-
+from .models import Aircraft, Airline
 
 def Homepage(request):
     msg = 'Hello, you are on the homepage.'
     return render(request, 'airlines/homepage.html',{'message':msg})
 
-def Airlines(request):
-    context = airlines_list
-    return render(request, 'airlines/airlines.html', {'airlines':context})
+def AirlinesView(request):
+    airlines = Airline.objects.all()
+    context = {'airlines': airlines}
+    return render(request, 'airlines/airlines.html', context)
 
-def Airline(request,pk):
-    Airline = None
-    for i in airlines_list: 
-        if i['id'] == pk: # Whatever is the primary key is equal to id val.
-            Airline = i
-    return render(request, 'airlines/airline.html', {'specific_airline': Airline})
 
-def AirCraft(request, pk):
-    AirCraft = None
-    for i in aircrafts_list: 
-        if i['id'] == pk:
-            AirCraft = i
-    return render(request, 'airlines/aircraft.html', {'aircraft':AirCraft})
+def AirlineView(request,pk):
+    airlineObj = Airline.objects.get(id=pk)
+    return render(request, 'airlines/airline.html', {'airlineObj': airlineObj})
 
-def AirCrafts(request):
-    context = aircrafts_list
-    return render(request, 'airlines/aircrafts.html', {'aircrafts':context})
+def AirCraftView(request, pk):
+    aircraftObj = Aircraft.objects.get(id=pk)
+    return render(request, 'airlines/aircraft.html', {'aircraftObj':aircraftObj})
+
+def AirCraftsView(request):
+    aircrafts = Aircraft.objects.all()
+    context = {'aircrafts': aircrafts}
+    return render(request, 'airlines/aircrafts.html', context)
 
 def ApiTokenAuth(request):
     return HttpResponse('Api Token Authorization ')
