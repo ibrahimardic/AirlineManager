499ecff4615d815a284524920150a82cb8ee7c05
ibrahimardic <ibrahimard2000@gmail.com>
2024-10-16 14:52:41 +0300

    Authenticated users and IsAuthenticated Permission defined

--- airlinemanager/settings.py
+++ airlinemanager/settings.py
@@ -127,7 +127,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ], # Only authenticated users can access to classes defined
 }
--- airlinemanager/urls.py
+++ airlinemanager/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('airlines.api.urls'))
+    path('', include('airlines.api.urls')),
+    path('api-auth', include('rest_framework.urls')), # Temporary login form
 ]
